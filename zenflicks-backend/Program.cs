5f3f1bacc977d3c240ad8bfb9b65854ee9540283
PieroMiranda1 <pieroms25@gmail.com>
2024-11-03 23:14:50 -0500

    chore: added configuration for programs.cs

--- zenflicks-backend/Program.cs
+++ zenflicks-backend/Program.cs
@@ -1,13 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using zenflicks_backend.shared.domain.repositories;
+using zenflicks_backend.shared.infrastructure.interfaces.ASP.configuration;
+using zenflicks_backend.shared.infrastructure.persistence.EFC.configuration;
+using zenflicks_backend.shared.infrastructure.persistence.EFC.repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new KebabCaseRouteNamingConvention());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+    });
+
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Configure Kebab Case Route Naming Convention
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new KebabCaseRouteNamingConvention());
+});
+
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Bounded Context Injection Configuration (one for each bounded context)
+//Example:
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
 var app = builder.Build();
+
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
