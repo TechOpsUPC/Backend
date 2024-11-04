b18fecb745b3c0e5ab31c4fc0921708d54f531d0
PieroMiranda1 <pieroms25@gmail.com>
2024-11-03 22:56:33 -0500

    feat: added AppDbContext

---
+++ zenflicks-backend/shared/infrastructure/persistence/EFC/configuration/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+using zenflicks_backend.shared.infrastructure.persistence.EFC.configuration.extensions;
+
+namespace zenflicks_backend.shared.infrastructure.persistence.EFC.configuration;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.UseSnakeCaseNamingConvention();
+    }
+    
+}
