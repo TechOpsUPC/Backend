ff27f24c80bd9ba51aa0c540b946a62fd38d3f11
PieroMiranda1 <pieroms25@gmail.com>
2024-11-04 00:50:56 -0500

    feat: added interfaces for repository and services

---
+++ ZenflicksPlatform.API/users/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using zenflicks_backend.shared.domain.repositories;
+using zenflicks_backend.users.Domain.Model.Aggregates;
+
+namespace zenflicks_backend.users.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> FindByIdAsync(string id);
+    
+    Task<User?> FindByUsernameAndPasswordAsync(string username, string password);
+}
