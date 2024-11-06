e1cccac0597458455ecbdb9ece1e83af1e324771
DiegoR434 <u202123548@upc.edu.pe>
2024-11-06 00:38:16 -0500

    feat: added event infrastructure files

---
+++ ZenflicksPlatform.API/events/Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using zenflicks_backend.events.Domain.Model.Aggregates;
+using zenflicks_backend.events.Domain.Repositories;
+using zenflicks_backend.shared.infrastructure.persistence.EFC.configuration;
+using zenflicks_backend.shared.infrastructure.persistence.EFC.repositories;
+
+namespace zenflicks_backend.events.Infrastructure.Repositories;
+
+public class EventRepository : BaseRepository<Event>, IEventRepository
+{
+    public EventRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Event?> FindByIdAsync(int id)
+    {
+        return await Context.Set<Event>().FirstOrDefaultAsync(u=>u.Id == id);
+    }
+
+    public async Task<Event?> FindByTittleAsync(string tittle)
+    {
+        return await Context.Set<Event>().FirstOrDefaultAsync(u=> u.tittle == tittle);
+    }
+}
