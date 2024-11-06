94ad77cf7d1f700564e76558916e8f025ed884b2
DiegoR434 <u202123548@upc.edu.pe>
2024-11-06 00:33:44 -0500

    feat: Added Event domain files

---
+++ ZenflicksPlatform.API/events/Domain/Model/Commands/CreateEventCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace zenflicks_backend.events.Domain.Model.Commands;
+
+public record CreateEventCommand ( string contentId,
+string tittle,
+string description,
+string date,
+string address,
+string creatorId
+    );
