fef3c1abfeecb167269f4c8c48779fbcca71bb92
PieroMiranda1 <pieroms25@gmail.com>
2024-11-22 21:18:55 -0500

    feat: added forum resource and entity assembler

---
+++ ZenflicksPlatform.API/forums/Interfaces/REST/Transform/ForumResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using zenflicks_backend.forums.Domain.Model.Aggregates;
+using zenflicks_backend.forums.Interfaces.REST.Resources;
+
+namespace zenflicks_backend.forums.Interfaces.REST.Transform;
+
+public static class ForumResourceFromEntityAssembler
+{
+    public static ForumResource ToResourceFromEntity(Forum entity) =>
+        new ForumResource(entity.Id, entity.Title, entity.CommentBy, entity.Comment,
+            entity.Image, entity.Score);
+}
