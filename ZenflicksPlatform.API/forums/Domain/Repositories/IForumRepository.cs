9666a2603295a351f9e42fd86cbefd1617b75eb2
PieroMiranda1 <pieroms25@gmail.com>
2024-11-22 19:33:46 -0500

    feat: added interfaces for repositories and services

---
+++ ZenflicksPlatform.API/forums/Domain/Repositories/IForumRepository.cs
@@ -0,0 +1,12 @@
+﻿using zenflicks_backend.forums.Domain.Model.Aggregates;
+using zenflicks_backend.shared.domain.repositories;
+
+namespace zenflicks_backend.forums.Domain.Repositories;
+
+public interface IForumRepository : IBaseRepository<Forum>
+{
+    Task<Forum?> FindByIdAsync(int id);
+    
+    Task<IEnumerable<Forum>> GetAllAsync();
+    
+}
---
+++ ZenflicksPlatform.API/forums/Domain/Services/IForumQueryService.cs
@@ -0,0 +1,11 @@
+﻿using zenflicks_backend.forums.Domain.Model.Aggregates;
+using zenflicks_backend.forums.Domain.Model.Queries;
+
+namespace zenflicks_backend.forums.Domain.Services;
+
+public interface IForumQueryService
+{
+    Task<Forum> Handle(GetForumByIdQuery query);
+    
+    Task<IEnumerable<Forum>> Handle(GetAllForumsQuery query);
+}
