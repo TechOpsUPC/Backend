c8d3536f3179f003a1c82a3ca734b948e15a29d7
PieroMiranda1 <pieroms25@gmail.com>
2024-11-22 19:22:41 -0500

    feat: added forum entity, command and queries

---
+++ ZenflicksPlatform.API/forums/Domain/Model/Aggregates/Forum.cs
@@ -0,0 +1,31 @@
+﻿using zenflicks_backend.forums.Domain.Model.Commands;
+
+namespace zenflicks_backend.forums.Domain.Model.Aggregates;
+
+public class Forum
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string CommentBy { get; private set; }
+    public string Comment { get; private set; }
+    public string Image { get; private set; }
+    public int Score { get; private set; }
+    
+    protected Forum()
+    {
+        this.Title = string.Empty;
+        this.CommentBy = string.Empty;
+        this.Comment = string.Empty;
+        this.Image = string.Empty;
+        this.Score = 0;
+    }
+    
+    public Forum(CreateForumCommand command)
+    {
+        this.Title = command.Title;
+        this.CommentBy = command.CommentBy;
+        this.Comment = command.Comment;
+        this.Image = command.Image;
+        this.Score = 0;
+    }
+}
---
+++ ZenflicksPlatform.API/forums/Domain/Model/Commands/CreateForumCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace zenflicks_backend.forums.Domain.Model.Commands;
+
+public record CreateForumCommand(string Title, string CommentBy, string Comment, string Image, int Score);
